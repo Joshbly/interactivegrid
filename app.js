1d67d839073b30361bd08fcc1ca5617c17532083
Joshbly <36315710+Joshbly@users.noreply.github.com>
2023-09-01 13:16:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 // app.js
 
 const tiles = [];
-const grid = document.getElementById('grid');
+let grid = document.getElementById('grid');
+let numTiles = 100;
 
-for (let i = 0; i < 100; i++) {
-    const newTile = document.createElement('div');
-    newTile.className = "tile";
-    newTile.onclick = function() {
-        this.className = "tile";
-        clearTimeout(autoBlackTurner);
-        autoBlackTurner = setTimeout(turnRandomTileBlack, 2000);
+function createTiles() {
+    for (let i = 0; i < numTiles; i++) {
+        const newTile = document.createElement('div');
+        newTile.className = "tile";
+        newTile.onclick = function() {
+            this.className = "tile";
+            clearTimeout(autoBlackTurner);
+            autoBlackTurner = setTimeout(turnRandomTileBlack, 2000);
+        }
+        grid.appendChild(newTile);
+        tiles.push(newTile);
     }
-    grid.appendChild(newTile);
-    tiles.push(newTile);
 }
+
+function removeTiles() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    tiles.length = 0;
+}
+
+function adjustTiles() {
+    let viewportWidth = window.innerWidth;
+    numTiles = Math.floor(viewportWidth / 100); // Adjust this to change the number of tiles per row based on the viewport width
+    removeTiles();
+    createTiles();
+}
+
+window.onresize = adjustTiles;
 
 function turnRandomTileBlack() {
     tiles[Math.floor(Math.random() * tiles.length)].className = "tile black";
 }
 
 let autoBlackTurner = setInterval(turnRandomTileBlack, 2000);
+
+adjustTiles();
